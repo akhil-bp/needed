17eb3ac118ae6a91014d7da193a60a4ca136c4fa
akhil <40385721+akhil-bp@users.noreply.github.com>
2019-09-18 10:28:38 +0530

    Update check filename.js

--- check filename.js
+++ check filename.js
@@ -1,7 +1,22 @@
- var re = /(?:\.([^.]+))?$/;
- var ext = re.exec(fileName)[1];
- // console.log(ext,"sfsf")
- var arr = ["png", "jpg", "jpeg", "PNG", "JPG", "JPEG"];
- if (arr.includes(ext) === false) {
- res.json({ success: false, respomse: "extension not supported" })
- }
+var re = /(?:\.([^.]+))?$/;
+        var ext = re.exec(pic)[1];
+        // console.log(ext,"sfsf")
+        var arr = ["png", "jpg", "jpeg", "PNG", "JPG", "JPEG"];
+        if (arr.includes(ext) === false) {
+            res.json({ success: false, respomse: "extension not supported" })
+        }
+        else {
+            var edituser = await userService.updateUser({ _id: o_id },
+                {
+                    $set:
+                    {
+                        imagefile: pic
+                    }
+                });
+            if (edituser) {
+                fs.unlink('./public/profilePics/' + oldImage, (err) => {
+                    console.log(err);
+                });
+            }
+            res.json({ success: true, image: pic });
+        }
